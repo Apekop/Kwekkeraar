9ec98b318ad8d20e26eee96e4f2162469665df63
Bram <brambruinsma@gmail.com>
2016-04-17 22:52:59 +0200

    Login meuk

--- src/Beans/Login.java
+++ src/Beans/Login.java
@@ -1,7 +1,42 @@
 package Beans;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
 /**
  * Created by bram_ on 16-4-2016.
  */
+@ManagedBean
+@SessionScoped
 public class Login {
+    String gebruikersnaam;
+    String wachtwoord;
+
+    public String CheckValidUser(){
+        if(gebruikersnaam.equals("Bram") &&
+                wachtwoord.equals("Bram")){
+            System.out.println("success");
+            return "success.jsp";
+        }
+        else{
+            System.out.println("fail");
+            return "/fail.jsp";
+        }
+    }
+
+    public String getGebruikersnaam() {
+        return gebruikersnaam;
+    }
+
+    public void setGebruikersnaam(String gebruikersnaam) {
+        this.gebruikersnaam = gebruikersnaam;
+    }
+
+    public String getWachtwoord() {
+        return wachtwoord;
+    }
+
+    public void setWachtwoord(String wachtwoord) {
+        this.wachtwoord = wachtwoord;
+    }
 }
--- web/fail.jsp
+++ web/fail.jsp
@@ -11,6 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-
+<p>fail</p>
 </body>
 </html>
